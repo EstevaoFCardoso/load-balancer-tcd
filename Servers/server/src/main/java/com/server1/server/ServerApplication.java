49fce2376f985ae444805be19b0c5d096004486a
Estêvão Filipe <estevaof10@gmail.com>
2023-01-06 20:10:43 -0300

    criando controller que centralizará as req

--- Servers/server/src/main/java/com/server1/server/ServerApplication.java
+++ Servers/server/src/main/java/com/server1/server/ServerApplication.java
@@ -1,22 +1,23 @@
 package com.server1.server;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 @SpringBootApplication
-@RestController
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String index(){
-		return "Server 1: Req GET";
-
 	}
 
 }
---
+++ Servers/server/src/main/java/com/server1/server/controller/ServerController.java
@@ -0,0 +1,23 @@
+package com.server1.server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+
+    @GetMapping("/get")
+    public ResponseEntity<Object> testeGet(){
+        return ResponseEntity.status(HttpStatus.OK).body("Server 1: Req GET");
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<Object> testePost(@RequestBody String message){
+        return ResponseEntity.status(HttpStatus.OK).body("Server 1: Req POST");
+    }
+
+}
