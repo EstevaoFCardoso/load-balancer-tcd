6e9d13edd32960a45b8ddd33835468ca0d527751
Estêvão Filipe <estevaof10@gmail.com>
2023-01-06 20:18:15 -0300

    criando controller que centralizará as req do servidor 3

---
+++ Servers/server3/src/main/java/com/server3/controller/ServerController.java
@@ -0,0 +1,23 @@
+package com.server3.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+
+    @GetMapping("/get")
+    public ResponseEntity<Object> testeGet(){
+        return ResponseEntity.status(HttpStatus.OK).body("Server 3: Req GET");
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<Object> testePost(@RequestBody String message){
+        return ResponseEntity.status(HttpStatus.OK).body("Server 3: Req POST");
+    }
+
+}
